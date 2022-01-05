7996f62a828581657d33fc8ba2c7f243df5f8557
SujayPrabhu96 <prabhusujay@gmail.com>
2022-01-05 23:28:23 +0530

    Add Project and Listener classes

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,25 @@
+// Project class
+enum ProjectStatus {
+  Active,
+  Finished,
+}
+
+class Project {
+  constructor(
+    public id: string,
+    public title: string,
+    public description: string,
+    public people: number,
+    public status: ProjectStatus
+  ) {}
+}
+
 // Project State Management
+type Listener = (items: Project[]) => void;
 
 class ProjectState {
-  private listeners: any[] = [];
-  private projects: any[] = [];
+  private listeners: Listener[] = [];
+  private projects: Project[] = [];
   private static instance: ProjectState;
 
   static getInstance() {
@@ -13,17 +30,19 @@ class ProjectState {
     return this.instance;
   }
 
-  addListener(listenerFn: Function) {
+  addListener(listenerFn: Listener) {
     this.listeners.push(listenerFn);
   }
 
   addProject(title: string, description: string, people: number) {
-    const newProject = {
-      id: Math.random().toString(), // Math.Random() should be changed, never suggested for ids
+    // Math.Random() should be changed, never suggested for ids
+    const newProject = new Project(
+      Math.random().toString(),
       title,
       description,
       people,
-    };
+      ProjectStatus.Active
+    );
     this.projects.push(newProject);
     for (const listenerFn of this.listeners) {
       listenerFn(this.projects.slice());
@@ -96,7 +115,7 @@ class ProjectList {
   templateElement: HTMLTemplateElement;
   hostElement: HTMLDivElement;
   element: HTMLElement;
-  assignedProjects: any[];
+  assignedProjects: Project[];
 
   constructor(private type: "active" | "finished") {
     this.templateElement = document.getElementById(
@@ -112,7 +131,7 @@ class ProjectList {
     this.element = importedNode.firstElementChild as HTMLElement;
     this.element.id = `${this.type}-projects`;
 
-    projectState.addListener((projects: any[]) => {
+    projectState.addListener((projects: Project[]) => {
       this.assignedProjects = projects;
       this.renderProjects();
     });
@@ -197,13 +216,13 @@ class ProjectInput {
     };
 
     const descriptionValidatable: Validatable = {
-      value: enteredTitle,
+      value: enteredDescription,
       required: true,
       minLength: 10,
     };
 
     const peopleValidatable: Validatable = {
-      value: enteredTitle,
+      value: enteredPeople,
       required: true,
       min: 1,
       max: 5,
